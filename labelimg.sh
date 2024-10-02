1433c2dfdebc2d77951989e46d28e2ff720b77f0
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2024-10-02 09:01:03 +0530

    Add files via upload

---
+++ labelimg.sh
@@ -0,0 +1,12 @@
+sudo apt-get install pyqt5-dev-tools
+
+sudo pip3 install labelimg
+
+sudo rm -rf /usr/local/lib/python3.11/dist-packages/libs/canvas.py
+
+sudo mv canvas.py /usr/local/lib/python3.11/dist-packages/libs
+
+sudo rm -rf /usr/local/lib/python3.11/dist-packages/labelImg/labelImg.py
+
+sudo mv labelImg.py /usr/local/lib/python3.11/dist-packages/labelImg
+
